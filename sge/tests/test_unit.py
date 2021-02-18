08a5d6d0c4296c27e721dc36c8a3f92ac6c61d50
rmmbear <rmmbear@users.noreply.github.com>
2021-02-18 07:20:55 +0100

    Create subclass of APISession for testing

--- sge/tests/test_unit.py
+++ sge/tests/test_unit.py
@@ -3,19 +3,117 @@ import time
 import logging
 import datetime
 
-import http.cookiejar
+from typing import Dict
 from urllib.parse import urlparse
 
 import pytest
+import requests
 import sqlalchemy.orm
 #from sqlalchemy import Session
 os.environ["FLASK_ENV"] = "development"
-os.environ["FLASK_DB_PATH"] = ""
 
 from sge import db
 from sge import steam_games_exporter as SGE
-LOGGER = logging.getLogger()
+LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
+
+TH = logging.StreamHandler()
+TH.setLevel(logging.DEBUG)
+TH.setFormatter(SGE.LOG_FORMAT)
+LOGGER.addHandler(TH)
+
+
+JSON_TEMPLATE_PROFILE = "{\"response\": {\"game_count\": %d, \"games\": [%s]}}"
+JSON_TEMPLATE_GAME = """{
+    "appid": %d,
+    "name": "%s",
+    "img_icon_url": "adc83b19e793491b1c6ea0fd8b46cd9f32e592fc",
+    "img_logo_url": "adc83b19e793491b1c6ea0fd8b46cd9f32e592fc",
+    "has_community_visible_stats": true,
+    "playtime_forever": 0,
+    "playtime_windows_forever": 0,
+    "playtime_mac_forever": 0,
+    "playtime_linux_forever": 0
+}"""
+JSON_TEMPLATE_GAMEINFO = """{
+    '%d':{
+        "success": true,
+        "data":{
+            "type": "game",
+            "name": "%s",
+            "steam_appid": %d,
+            "required_age": 0,
+            "controller_support": "full",
+            "supported_languages": "English<strong>*</strong>, French, Spanish - Spain, Korean<br><strong>*</strong>languages with full audio support",
+            "developers": ["developer 1", "developer2"],
+            "publishers": ["publisher 1"],
+            "platforms": {"windows": true, "mac": true, "linux": true},
+            "categories": [{"id": 2,"description": "Single-player"},
+                           {"id": 22, "description": "Steam Achievements"},
+                           {"id": 28, "description": "Full controller support"},
+                           {"id": 29, "description": "Steam Trading Cards"},
+                           {"id": 23, "description": "Steam Cloud"},
+                           {"id": 43, "description": "Remote Play on TV"}],
+            "genres": [{"id": "23", "description": "Indie"},
+                       {"id": "3", "description": "RPG"}],
+            "release_date": {"coming_soon": false, "date": "00 Month, Year"},
+            "content_descriptors": {"ids": [], "notes": None}
+        }
+    }
+}"""
+
+
+class DummyAPISession(SGE.APISession):
+    GENERATE_GAMES_NUM = 999
+
+    def query(self, prepared_query: requests.PreparedRequest, *args, **kwargs) -> requests.Response:
+        """Return dummy json as requests.Response."""
+        url = urlparse(str(prepared_query.url))
+        query = dict(pair.split("=") for pair in url.query.lower().split("&")) #type: Dict[str, str]
+        if url.netloc in SGE.API_STORE_URL:
+            appid = int(query["appids"])
+            LOGGER.debug("Querying store with appid=%s", appid)
+            content = self.fetch_dummy_game_info(appid).encode()
+        elif url.netloc in SGE.API_GAMES_URL:
+            steamid = int(query["steamid"])
+            LOGGER.debug("Querying profile with steamid=%s", steamid)
+            content = self.fetch_dummy_steam_profile(steamid).encode()
+        else:
+            raise ValueError("UNKNOWN ENDPOINT: %s", prepared_query.url)
+
+        # this is not the proper way of creating a new response
+        # but it will work well enough for our purpose
+        response = requests.Response()
+        response.encoding = "utf-8"
+        response._content = content
+        response._content_consumed = True
+        response.status_code = 200
+        response.reason = "OK"
+        return response
+
+
+    def fetch_dummy_steam_profile(self, steamid: int) -> str:
+        """Generate cls.GENERATE_GAMES_NUM fake game entries.
+        Return those entries formatted as steam API json response.
+        """
+        games = []
+        for i in range(1, self.GENERATE_GAMES_NUM + 1):
+            games.append(JSON_TEMPLATE_GAME % (i, f"App {i}"))
+
+        return JSON_TEMPLATE_PROFILE % (len(games), ", ".join(games))
+
+
+    def fetch_dummy_game_info(self, appid: int) -> str:
+        """"""
+        return JSON_TEMPLATE_GAMEINFO % (appid, f"App {appid}", appid)
+
+
+
+@pytest.fixture
+def api_session_fixture(monkeypatch):
+    monkeypatch.setattr(SGE, "APISession", DummyAPISession)
+    yield
+
 
 @pytest.fixture
 def app_client_fixture():
@@ -29,32 +127,15 @@ def app_client_fixture():
 
 @pytest.fixture
 def db_session_fixture(monkeypatch):
-    monkeypatch.setattr(SGE.db.SESSION, "remove", lambda: None)
     monkeypatch.setattr(SGE, "db_init", lambda: None)
-    db.init("sqlite:///:memory:")
+    db.init(":memory:")
     yield db.SESSION()
     sqlalchemy.orm.close_all_sessions()
 
 
-def generate_fake_game_info(maxid: int, db_session):
-    gameinfo = []
-    for i in range(1, maxid+1):
-        gameinfo.append(
-            db.GameInfo(
-                appid=i, name=f"dummy app {i}", developers=f"dev 1,\ndev 2 for app {i}",
-                publishers=f"publisher 1,\npublisher 2 for app {i}", on_linux=True, on_mac=True,
-                on_windows=False, categories=f"category 1,\ncategory 2 for app {i}",
-                genres=f"genre 1,\ngenre 2 for app {i}",
-                release_date=str(datetime.datetime.fromtimestamp(0)), timestamp=int(time.time())
-            )
-        )
-    db_session.bulk_save_objects(gameinfo)
-    db_session.commit()
-
-
-def test_routing(app_client_fixture):
+def test_routing(app_client_fixture, db_session_fixture):
     """"""
-    client, app = app_client_fixture
+    client, _ = app_client_fixture
     # POST to index not allowed
     resp = client.post("/tools/steam-games-exporter/")
     assert resp.status_code == 405
@@ -99,42 +180,34 @@ def test_routing(app_client_fixture):
     #TODO: test /tools/steam-games-exporter/export with an invalid export format
 
 
-def test_extended_export(app_client_fixture, db_session_fixture, monkeypatch):
-    client, app = app_client_fixture
+def test_extended_export(api_session_fixture, app_client_fixture, db_session_fixture):
+    client, _ = app_client_fixture
     db_session = db_session_fixture
 
-    # Generate fake profile data, patch that to query_profile
-    fake_user_data = [
-        {"appid":x, "name":f"{x}", "playtime_forever":x, "playtime_windows_forever":x,
-         "playtime_mac_forever":x, "playtime_linux_forever":x} for x in range(1, 1000)
-    ]
-    monkeypatch.setattr(
-        SGE.APISession, "query_profile",
-        lambda self, steamid: {"game_count": 1000, "games": fake_user_data}
-    )
     # set dummy steamid to prevent redirecting to index
     with client.session_transaction() as app_session:
         app_session["steamid"] = 1234567890
 
     ### POST: valid request, missing game info -> user shown info about pending export
     resp = client.post(
-        "/tools/steam-games-exporter/export?export", data={"format": "csv", "include-gameinfo": True}
+        "/tools/steam-games-exporter/export?export",
+        data={"format": "csv", "include-gameinfo": True}
     )
     assert resp.status_code == 202
     assert not resp.headers.get("Location")
     assert "Items added to the queue, return later" in resp.get_data().decode()
-
-
-    generate_fake_game_info(1000, db_session)
-    db_session.query(db.Queue).delete() #clear the queue manually
+    assert "job" in [cookie.name for cookie in client.cookie_jar]
 
     ### GET: job cookie present from last request, game info available for export
+    generate_fake_game_info(1000, db_session)
+    db_session.query(db.Queue).delete() #clear the queue manually
     resp = client.get("/tools/steam-games-exporter/export")
     assert resp.status_code == 200
     assert not resp.headers.get("Location")
     assert "attachment" in resp.headers.get("Content-Disposition")
 
-
+    with client.session_transaction() as app_session:
+        app_session["steamid"] = 1234567890
     ### POST: game info already available, do not queue anything, export in one step
     resp = client.post(
         "/tools/steam-games-exporter/export?export", data={"format": "xlsx", "include-gameinfo": True}
@@ -147,5 +220,5 @@ def test_extended_export(app_client_fixture, db_session_fixture, monkeypatch):
         # ~ version=1, name="job", value=fake_user.job_uuid, port=80, port_specified=False, domain="",
         # ~ domain_specified=False, domain_initial_dot=False, path="/tools/steam-games-exporter",
         # ~ path_specified=True, secure=False, expires=int(time.time() + 60*60*24), discard=False,
-        # ~ comment=None, comment_url=None, rest=None
+        # ~ comment=None, comment_url=None, rest={'HttpOnly': None, 'SameSite': 'Lax'}
     # ~ )
