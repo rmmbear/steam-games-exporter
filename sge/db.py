3496fe2c9ff5431ee0b254443a8d41e2c2a03e04
rmmbear <rmmbear@users.noreply.github.com>
2021-02-18 07:31:48 +0100

    Add fields to GameInfo, implement a factory method for GameInfo, fix some db_init issues

--- sge/db.py
+++ sge/db.py
@@ -1,11 +1,16 @@
+import re
 import json
 import time
 import uuid
+import logging
 
 import sqlalchemy
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 
+LOGGER = logging.getLogger(__name__)
+
+RE_SIMPLE_HTML = re.compile("<.*?>")
 ORM_BASE: DeclarativeMeta = declarative_base()
 
 #TODO: naming collision with all the networking/server stuff, find a better name
@@ -17,7 +22,7 @@ class Request(ORM_BASE):
     export_format = sqlalchemy.Column(sqlalchemy.String)
     generated_file = sqlalchemy.Column(sqlalchemy.String, nullable=True)
 
-    def __init__(self, games_json: dict, export_format: str):
+    def __init__(self, games_json: dict, export_format: str) -> None:
         if export_format not in ["ods", "xls", "xlsx", "csv"]:
             raise ValueError(f"Export format not recognized {export_format}")
 
@@ -32,6 +37,8 @@ class Queue(ORM_BASE):
     __tablename__ = "games_queue"
     appid = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     job_uuid = sqlalchemy.Column(sqlalchemy.String)
+    app_name = sqlalchemy.Column(sqlalchemy.String)
+    timestamp = sqlalchemy.Column(sqlalchemy.Integer)
     scrape = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
     #^ scrape the info from store instead of using the api
 
@@ -45,16 +52,74 @@ class GameInfo(ORM_BASE):
     on_linux = sqlalchemy.Column(sqlalchemy.Boolean)
     on_mac = sqlalchemy.Column(sqlalchemy.Boolean)
     on_windows = sqlalchemy.Column(sqlalchemy.Boolean)
+    supported_languages = sqlalchemy.Column(sqlalchemy.String)
+    controller_support = sqlalchemy.Column(sqlalchemy.String)
+    age_gate = sqlalchemy.Column(sqlalchemy.Integer)
     categories = sqlalchemy.Column(sqlalchemy.String) #(csv, sep=",\n")
     genres = sqlalchemy.Column(sqlalchemy.String) #(csv, sep=",\n")
     release_date = sqlalchemy.Column(sqlalchemy.String)
-    timestamp = sqlalchemy.Column(sqlalchemy.Integer)
+    #content_descriptors
+    #^this field seems to always be set to null,
+    # even when that information is normally present on the store page
+    timestamp = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    unavailable = sqlalchemy.Column(sqlalchemy.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return "<GameInfo(appid='{}', name='{}', ... timestamp='{}', unavailable='{}')>".format(
+            self.appid, self.name, self.timestamp, self.unavailable
+        )
+
+    @classmethod
+    def from_json(cls,appid: int, info_json: dict) -> None:
+        #{'<appid>': {'success': <bool>, 'data': {'steam_appid':<steam_appid>, ...}}}
+        # info_json = json['<appid>']['data']
+        #NOTE: steam_appid and appid are not guaranteed to be the same
+        # (this is mostly the case for 'bonus' apps bundled with purchase)
+        # (for example, a single-player game can have a multi-player mode as a deparate app)
+        # (this app's steam_appid will actually be the main app's appid)
+        # (I assume this is to make it possible to have both apps have the same store page)
+        appid = appid
+        name = info_json["name"]
+        if "developers" in info_json:
+            developers = ",\n".join(info_json["developers"])
+        else:
+            developers = "N/A"
+        publishers = ",\n".join(info_json["publishers"])
+        on_linux = info_json["platforms"]["linux"]
+        on_mac = info_json["platforms"]["mac"]
+        on_windows = info_json["platforms"]["windows"]
+        supported_languages = info_json["supported_languages"].replace("<br>", "\n")
+        supported_languages = re.sub(RE_SIMPLE_HTML, "", supported_languages)
+        controller_support = info_json.get("controller_support", "")
+        age_gate = info_json["required_age"]
+        if "categories" in info_json:
+            categories = ",\n".join(
+                [category["description"] for category in info_json["categories"]])
+        else:
+            categories = "N/A"
+        if "genres" in info_json:
+            genres = ",\n".join(genre["description"] for genre in info_json["genres"])
+        else:
+            genres = "N/A"
+        release_date = info_json["release_date"]["date"]
+        timestamp = int(time.time())
+        unavailable = False
+
+        kwargs = locals()
+        del kwargs["cls"], kwargs["info_json"]
+        new_obj = cls(**kwargs)
+        return new_obj
+
 
 
 SESSIONMAKER = sessionmaker(autocommit=False, autoflush=False)
 SESSION = scoped_session(SESSIONMAKER)
 
-def init(url: str) -> None:
-    engine = sqlalchemy.create_engine(url, echo=False)
+def init(path: str) -> None:
+    LOGGER.info("Performing db init")
+    if path in ("", ":memory:"):
+        engine = sqlalchemy.create_engine(f"sqlite:///{path}", echo=False, connect_args={'check_same_thread':False}, poolclass=sqlalchemy.pool.StaticPool)
+    else:
+        engine = sqlalchemy.create_engine(f"sqlite:///{path}", echo=False, connect_args={'check_same_thread':False})
     SESSIONMAKER.configure(bind=engine)
     ORM_BASE.metadata.create_all(bind=engine)
