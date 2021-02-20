c4a31f5f60b8c20364846ac5b3c588ea5edd6aaf
rmmbear <rmmbear@users.noreply.github.com>
2021-02-20 07:55:23 +0100

    Remove reference to deleted config

--- run.py
+++ run.py
@@ -6,7 +6,6 @@ import sys
 # uwsgi emperor launches the app from within the venv
 # so path from which sge can be imported must be added manually
 sys.path.append(os.path.realpath(__file__).rsplit("/", maxsplit=1)[0])
-from sge import config
 from sge.steam_games_exporter import create_app
 
-app = create_app(config)
+APP = create_app()
