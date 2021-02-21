dfe9d89dbf66d85b830eb0e126325e517603ece3
rmmbear <rmmbear@users.noreply.github.com>
2021-02-21 22:46:32 +0100

    enable smtp logging handler

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 """
 import os
 import sys
+import logging
+import logging.handlers
 
 try:
     # https://uwsgi-docs.readthedocs.io/en/latest/PythonModule.html
@@ -13,9 +15,18 @@ except ImportError:
 # uwsgi emperor launches the app from within the venv
 # so path from which sge can be imported must be added manually
 sys.path.append(os.path.realpath(__file__).rsplit("/", maxsplit=1)[0])
-from sge.steam_games_exporter import create_app
+from sge.steam_games_exporter import create_app, cleanup
 
 APP = create_app()
 if "uwsgi" in locals():
-    uwsgi.register_signal(10, "", SGE.cleanup)
+    LOGGER = logging.getLogger(__name__)
+    LOGGER = LOGGER.setLevel(logging.INFO)
+    MAIL_HANDLER = logging.handlers.SMTPHandler(
+        "localhost", fromaddr="root@untextured.space", toaddrs="root@localhost",
+        subject="[Steam Games Exporter]", secure=tuple())
+    MAIL_HANDLER.setLevel(logging.INFO)
+    LOGGER.addHandler(MAIL_HANDLER)
+
+    uwsgi.register_signal(10, "", cleanup)
     uwsgi.add_cron(10, 0, 1, -1, -1, -1)
+    LOGGER.info("Steam Games Exporter started")
