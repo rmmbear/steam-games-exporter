ace3dd574b299bda9858dfa74a4ef01c4b1dc5af
rmmbear <rmmbear@users.noreply.github.com>
2021-02-21 23:04:38 +0100

    Raise level and attach mail handler to exporter's logger

--- run.py
+++ run.py
@@ -15,10 +15,13 @@ except ImportError:
 # uwsgi emperor launches the app from within the venv
 # so path from which sge can be imported must be added manually
 sys.path.append(os.path.realpath(__file__).rsplit("/", maxsplit=1)[0])
-from sge.steam_games_exporter import create_app, cleanup
+from sge.steam_games_exporter import create_app, cleanup, LOGGER as SGE_LOGGER
 
 APP = create_app()
 if "uwsgi" in locals():
+    uwsgi.register_signal(10, "", cleanup)
+    uwsgi.add_cron(10, 0, 1, -1, -1, -1)
+
     LOGGER = logging.getLogger(__name__)
     LOGGER.setLevel(logging.INFO)
     MAIL_HANDLER = logging.handlers.SMTPHandler(
@@ -26,7 +29,6 @@ if "uwsgi" in locals():
         subject="[Steam Games Exporter]", secure=tuple())
     MAIL_HANDLER.setLevel(logging.INFO)
     LOGGER.addHandler(MAIL_HANDLER)
-
-    uwsgi.register_signal(10, "", cleanup)
-    uwsgi.add_cron(10, 0, 1, -1, -1, -1)
-    LOGGER.info("Steam Games Exporter started")
+    LOGGER.info("Steam Games Exporter started successfully in uwsgi mode")
+    MAIL_HANDLER.setLevel(logging.ERROR)
+    SGE_LOGGER.addHandler(MAIL_HANDLER)
